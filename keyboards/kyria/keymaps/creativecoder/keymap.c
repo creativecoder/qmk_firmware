88a6c8fd56ecc6227db62ac161d0ccf7e2f093d6
Grant Kinney <hi@grant.mk>
2021-08-13 11:45:33 -0500

    Keymap updates
    - Move thumb keys in
    - Add custom key for copying a browser url

--- keyboards/kyria/keymaps/creativecoder/keymap.c
+++ keyboards/kyria/keymaps/creativecoder/keymap.c
@@ -26,7 +26,6 @@ uint16_t app_switcher_timer = 0;
 #include "oled.h"
 #include "encoders.h"
 
-#define U_NP KC_NO // key is not present
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
 #define XXX KC_NO
@@ -52,6 +51,8 @@ XXX, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, XXX, 
 
 enum custom_keycodes {
     RELEASE_LGUI = SAFE_RANGE,
+    CPY_URL,
+    OPN_URL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,51 +60,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,                                                                                          KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,
       LCTL_T(KC_A),      LALT_T(KC_R),      LGUI_T(KC_S),      LSFT_T(KC_T),      KC_G,                                                                                          KC_M,              LSFT_T(KC_N),      LGUI_T(KC_E),      LALT_T(KC_I),      LCTL_T(KC_O),
       KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              LT(NUM, KC_LBRC),  U_NU,              U_NU,              LT(NAV, KC_RBRC),  KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
-                         RELEASE_LGUI,      U_NU,              LT(MEDIA, KC_ESC), LT(NAV, KC_SPC),   LT(MOUSE, KC_TAB), LT(SYM, KC_ENT),   LT(NUM, KC_BSPC),  LT(FUN, KC_DEL),   U_NU,              U_NU
+                                            RELEASE_LGUI,      LT(MEDIA, KC_ESC), LT(NAV, KC_SPC),   LT(MOUSE, KC_TAB), U_NU,              U_NU,              LT(SYM, KC_ENT),   LT(NUM, KC_BSPC),  LT(FUN, KC_DEL),   U_NU
     ),
     [NAV] = LAYOUT_creativecoder(
       RESET,   U_NA,    U_NA,    U_NA,    U_NA,                                        U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
       KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, U_NA,                                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
-      U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
-                        KC_TRNS, U_NA,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_BSPC, KC_DEL,  U_NU,    U_NA
+      U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  CPY_URL,
+                        U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    KC_ENT,  KC_BSPC, KC_DEL,  U_NU
     ),
     [MOUSE] = LAYOUT_creativecoder(
       RESET,   U_NA,    U_NA,    U_NA,    U_NA,                                        U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
       KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, U_NA,                                        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, U_NU,
-      U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, U_NU,
-                        U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
+      U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, U_NU,
+                        U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NU
     ),
     [MEDIA] = LAYOUT_creativecoder(
       RESET,   U_NA,    U_NA,    U_NA,    U_NA,                                        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
       KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, U_NA,                                        KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, U_NU,
       U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
-                        U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, U_NA,    KC_MUTE
+                        U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, KC_MUTE
     ),
     [MBO] = LAYOUT_creativecoder(
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_TRNS,                                     KC_TRNS, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
       U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-                        U_NA,    U_NA,    KC_BTN2, KC_BTN3, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2, U_NA,    U_NA
+                        U_NU,    KC_BTN2, KC_BTN3, KC_BTN1, U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NU
     ),
     [NUM] = LAYOUT_creativecoder(
       KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                                     U_NA,    U_NA,    U_NA,    U_NA,    RESET,
       KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                                      U_NA,    KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-                        U_NA,    U_NA,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA
+                        U_NU,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU
     ),
     [SYM] = LAYOUT_creativecoder(
       KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                     U_NA,    U_NA,    U_NA,    U_NA,    RESET,
       KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                                     U_NA,    KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-                        U_NA,    U_NA,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA
+                        U_NU,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU
     ),
     [FUN] = LAYOUT_creativecoder(
       KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,                                     U_NA,    U_NA,    U_NA,    U_NA,    RESET,
       KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK,                                     U_NA,    KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
       KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-                        U_NA,    U_NA,    KC_APP,  KC_SPC,  KC_TAB,  U_NA,    U_NA,    U_NA,    U_NA,    U_NA
+                        U_NU,    KC_APP,  KC_SPC,  KC_TAB,  U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU
     ),
 };
+
+// const key_override_t copy_url_key_override = ko_make_basic(MOD_MASK_SHIFT, CPY_URL, OPN_URL);
+
+// // This globally defines all key overrides to be used
+// const key_override_t **key_overrides = (const key_override_t *[]){
+// 	&copy_url_key_override,
+// 	NULL // Null terminate the array of overrides!
+// };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -111,6 +120,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed && is_app_switcher_active) {
             unregister_code(KC_LGUI);
             is_app_switcher_active = false;
+        }
+        break;
+    case CPY_URL:
+        if (record->event.pressed) {
+            tap_code16(LCMD(KC_L));
+            tap_code16(U_CPY);
+        }
+        break;
+    case OPN_URL:
+        if (record->event.pressed) {
+            tap_code16(LCMD(KC_L));
+            tap_code16(U_CPY);
+            tap_code16(LCMD(KC_T));
+            tap_code16(U_PST);
         }
         break;
     }
--- keyboards/kyria/keymaps/creativecoder/rules.mk
+++ keyboards/kyria/keymaps/creativecoder/rules.mk
@@ -1,3 +1,4 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+KEY_OVERRIDE_ENABLE = yes  # Enable key overrides (https://github.com/JonasGessner/qmk_firmware/blob/key-overrides/docs/feature_key_overrides.md)
