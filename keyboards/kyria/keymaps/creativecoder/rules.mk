da79701f1d8f04baeef8a5c5210d1d89e97137ee
Grant Kinney <hi@grant.mk>
2021-08-14 21:50:00 -0500

    Tap dance z key for Launchbar instant send

--- keyboards/kyria/keymaps/creativecoder/rules.mk
+++ keyboards/kyria/keymaps/creativecoder/rules.mk
@@ -1,3 +1,4 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+OLED_DRIVER_ENABLE = yes   # https://docs.qmk.fm/#/feature_oled_driver
+ENCODER_ENABLE = yes       # https://docs.qmk.fm/#/feature_encoders
+RGBLIGHT_ENABLE = yes      # https://docs.qmk.fm/#/feature_rgblight
+TAP_DANCE_ENABLE = yes     # https://docs.qmk.fm/#/feature_tap_dance
